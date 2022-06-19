792e29eefa10581ffd4fca406ee1a9861126543f
Deadpool <abcd162003@gmail.com>
2022-06-20 01:13:10 +0530

    added wss

--- server/core/settings.py
+++ server/core/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "channels",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "channels",
     "chat",
 ]
 
