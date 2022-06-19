22bf252269fc29194567d675dee8fc7a590abd4e
Deadpool <abcd162003@gmail.com>
2022-06-20 02:30:20 +0530

    asgi.py

--- server/core/asgi.py
+++ server/core/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 import django
-from channels.routing import get_asgi_application
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
-application = get_asgi_application()
+application = get_default_application()
